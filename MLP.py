e9b807b803e3715aced9dd02da43d596845091cf
BoulghobraYounes <younes.boulghobrapro@gmail.com>
2025-03-05 11:30:06 +0100

    Created forward propagation function to compute activations for each layer using Sigmoid activation function

--- MLP.py
+++ MLP.py
@@ -1,6 +1,8 @@
 import numpy as np
+from sklearn.datasets import make_blobs 
 
-def initialization(network: tuple) -> dict:
+
+def initialization(network: tuple) -> dict[str, np.ndarray]:
     parameters = {}
     L = len(network)
     for layer in range(1, L):
@@ -8,7 +10,28 @@ def initialization(network: tuple) -> dict:
         parameters[f"b{layer}"] = np.zeros((network[layer], 1))
     return parameters
 
+
+# Activation function
+def sigmoid(potential: np.ndarray) -> np.ndarray:
+    return 1 / (1 + np.exp(-potential))
+
+
+def forward_propagation(parameters: dict[str, np.ndarray], X: np.ndarray) -> dict[str, np.ndarray]:
+    activations = {'A0': X}
+    L = len(parameters) // 2
+    for layer in range(1, L + 1):
+        potential = np.dot(parameters[f"W{layer}"], activations[f"A{layer - 1}"]) + parameters[f"b{layer}"]
+        activations[f"A{layer}"] = sigmoid(potential)
+    return activations
+
+
+
 network = (2, 4, 4, 1)
 parameters = initialization(network)
-for key, value in parameters.items():
+
+# Generate random dataset : X(100, 2) y(100)
+X, y = make_blobs(n_samples=100, n_features=2, centers=2, random_state=0)
+X = X.T # X => (2, 100)
+activations = forward_propagation(parameters, X)
+for key, value in activations.items():
     print(key, value.shape)
